7b46637f29b46464d880223c4bd0bbbe43175dfd
Ashu Maurya <mauryaashu1994@gmail.com>
2021-07-04 10:30:28 +0530

    made code readable...

--- app/src/main/java/com/example/covidvaccination/VaccineDetailActivity.kt
+++ app/src/main/java/com/example/covidvaccination/VaccineDetailActivity.kt
@@ -8,11 +8,12 @@ import android.util.Log
 import android.view.View
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import kotlinx.android.synthetic.main.activity_vaccine_detail.*
+import com.example.covidvaccination.databinding.ActivityVaccineDetailBinding
 import org.json.JSONException
 import org.json.JSONObject
 import kotlin.collections.ArrayList
@@ -20,45 +21,62 @@ import kotlin.collections.ArrayList
 class VaccineDetailActivity : AppCompatActivity() {
 
     var vaccineModel = ArrayList<VaccineModel>()
-    private var adapter:VaccineAdapter? = null
-
+    private var adapter: VaccineAdapter? = null
+    private lateinit var binding: ActivityVaccineDetailBinding
+    var pincode: String? = null
+    var date: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_vaccine_detail)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_vaccine_detail)
 
-        supportActionBar!!.title= "Vaccination Centers"
-        supportActionBar!!.setDisplayShowHomeEnabled(true)
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar.apply {
+            title = "Vaccination Centers"
+            this!!.setDisplayHomeAsUpEnabled(true)
+            this!!.setDisplayHomeAsUpEnabled(true)
+        }
 
+        if (intent.hasExtra("Pincode")) {
+            pincode = intent.getStringExtra("Pincode")
+        }
+        if (intent.hasExtra("Date")) {
+            date = intent.getStringExtra("Date")
+        }
 
-        var pincode = intent.getStringExtra("Pincode")
-        var date = intent.getStringExtra("Date")
+        getAppointmentDetails(pincode!!, date!!)
 
-        getAppointmentDetails(pincode, date)
+        binding.apply {
+            etSearch.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
 
-        et_search.addTextChangedListener(object: TextWatcher{
-            override fun afterTextChanged(s: Editable?) {
+                    filter(s.toString())
+                }
 
-                filter(s.toString())
-            }
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
 
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    filter(s.toString())
+                }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                filter(s.toString())
-            }
-
-        })
+            })
+        }
 
     }
 
-    private fun filter(toString: String){
-        var filterList= ArrayList< VaccineModel>()
+    private fun filter(toString: String) {
+        var filterList = ArrayList<VaccineModel>()
 
-        for(item in vaccineModel){
-            if (item.centerAddress.toLowerCase().contains(toString.toLowerCase()) || item.centerName.toLowerCase().contains(toString.toLowerCase())){
+        for (item in vaccineModel) {
+            if (item.centerAddress.toLowerCase()
+                    .contains(toString.toLowerCase()) || item.centerName.toLowerCase()
+                    .contains(toString.toLowerCase())
+            ) {
                 filterList.add(item)
             }
         }
@@ -67,71 +85,77 @@ class VaccineDetailActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        if (item.itemId == android.R.id.home){
+        if (item.itemId == android.R.id.home) {
             vaccineModel.clear()
             finish()
-
 
 
         }
         return super.onOptionsItemSelected(item)
     }
 
-    private fun getAppointmentDetails(pincode: String, date: String){
-        val url ="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode="+pincode+"&date="+date
+    private fun getAppointmentDetails(pincode: String, date: String) {
+        val url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=" + pincode + "&date=" + date
         Log.i("URL", url.toString())
-        arc_loader.start()
+        binding.arcLoader.start()
         val queue = Volley.newRequestQueue(this)
 
 
         val request = JsonObjectRequest(
-            Request.Method.GET, url, null,{
-                response ->
+            Request.Method.GET, url, null, { response ->
 
-            try{
-                val centerArray = response.getJSONArray("sessions")
-                if (centerArray.length().equals(0)) {
-                    Toast.makeText(this, "No Vaccination Centers Found!!", Toast.LENGTH_SHORT)
-                        .show()
+                try {
+                    val centerArray = response.getJSONArray("sessions")
+                    if (centerArray.length().equals(0)) {
+                        Toast.makeText(this, "No Vaccination Centers Found!!", Toast.LENGTH_SHORT)
+                            .show()
 
+                    }
+
+                    for (i in 0 until centerArray.length()) {
+
+
+                        var jsonObject: JSONObject = centerArray.getJSONObject(i)
+                        var centerName: String = jsonObject.getString("name")
+                        var centerAddress: String = jsonObject.getString("address")
+                        var centerFromTime: String = jsonObject.getString("from")
+                        var centerToTime: String = jsonObject.getString("to")
+                        var feeType: String = jsonObject.getString("fee_type")
+                        var availableCapacity = jsonObject.getInt("available_capacity")
+                        var ageLimit = jsonObject.getInt("min_age_limit")
+                        var vaccineName = jsonObject.getString("vaccine")
+
+                        val center = VaccineModel(
+                            centerName,
+                            centerAddress,
+                            centerFromTime,
+                            centerToTime,
+                            feeType,
+                            ageLimit,
+                            vaccineName,
+                            availableCapacity
+                        )
+
+                        vaccineModel.add(center)
+
+
+                    }
+                    binding.apply {
+                        arcLoader.stop()
+                        arcLoader.visibility = View.GONE
+                        recyclerView.visibility = View.VISIBLE
+
+                        adapter = VaccineAdapter(vaccineModel)
+                        recyclerView.layoutManager = LinearLayoutManager(this@VaccineDetailActivity)
+                        recyclerView.adapter = adapter
+                    }
+
+                } catch (e: JSONException) {
+
+                    e.printStackTrace()
                 }
-
-                for (i in 0 until  centerArray.length()){
-
-
-                    var jsonObject: JSONObject =centerArray.getJSONObject(i)
-                    var centerName:String = jsonObject.getString("name")
-                    var centerAddress:String = jsonObject.getString("address")
-                    var centerFromTime:String = jsonObject.getString("from")
-                    var centerToTime:String = jsonObject.getString("to")
-                    var feeType:String = jsonObject.getString("fee_type")
-                    var availableCapacity = jsonObject.getInt("available_capacity")
-                    var ageLimit = jsonObject.getInt("min_age_limit")
-                    var vaccineName = jsonObject.getString("vaccine")
-
-                    val center = VaccineModel(centerName, centerAddress, centerFromTime, centerToTime, feeType, ageLimit, vaccineName, availableCapacity)
-
-                    vaccineModel.add(center)
-
-
-
-
-                }
-                arc_loader.stop()
-                arc_loader.visibility = View.GONE
-                recycler_view.visibility = View.VISIBLE
-
-                adapter = VaccineAdapter(vaccineModel)
-                recycler_view.layoutManager = LinearLayoutManager(this)
-                recycler_view.adapter = adapter
-
-            }catch (e: JSONException){
-
-                e.printStackTrace()
-            }
-        },
-            {
-                    error ->
+            },
+            { _ ->
 
                 Toast.makeText(this, "Failed To Load Data", Toast.LENGTH_SHORT).show()
             })
